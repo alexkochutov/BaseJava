857c60587cea13e265280bbb0b68cbc49a44bacd
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-04-25 13:44:00 +0400

    feat: Change signature of removeItem method (add index parameter)

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -15,7 +15,7 @@ public class ArrayStorage extends AbstractArrayStorage {
     }
 
     @Override
-    protected void insertItem(Resume r) {
+    protected void insertItem(Resume r, int index) {
         storage[size] = r;
     }
 
