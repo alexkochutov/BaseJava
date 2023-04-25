a3b9cd4d9805edbda3633e709b1c5bd2580dea0a
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-04-25 04:04:55 +0400

    feat: Add implementation of Storage interface

---
+++ src/com/basejava/webapp/storage/Storage.java
@@ -0,0 +1,19 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+public interface Storage {
+    void save(Resume r);
+
+    void update(Resume r);
+
+    void delete(String uuid);
+
+    void clear();
+
+    int size();
+
+    Resume get(String uuid);
+
+    Resume[] getAll();
+}
