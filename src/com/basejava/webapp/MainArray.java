c51c0a230d9952f8b4a4d77daf4263cd2c4439ac
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-04-28 23:30:15 +0400

    feat: Add some code style changes

--- src/com/basejava/webapp/MainArray.java
+++ src/com/basejava/webapp/MainArray.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainArray {
-    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
