a09e79932d5a188a5b0c14816e598f9babffed44
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-05-04 03:41:18 +0400

    feat: Add test saveOverflow to file AbstractArrayStorageTest.java for testing exception in Array related realisations

---
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,25 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.exception.StorageException;
+import com.basejava.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Test;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
+    public AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() {
+        storage.clear();
+        try {
+            for (int i = storage.size(); i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail();
+        }
+        storage.save(new Resume());
+    }
+}
