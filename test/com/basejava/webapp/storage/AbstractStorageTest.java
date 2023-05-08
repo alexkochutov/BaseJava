08a26d7ba3a5b1e01db75accf1e20240a26e2fe0
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-05-08 05:27:06 +0400

    feat: Change access modifiers from "private" to "protected" for 	- RESUME_1, RESUME_2, RESUME_3, RESUME_4 constants for access from nested test-class (MapStorageTest.java)

--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -15,10 +15,10 @@ public abstract class AbstractStorageTest {
     private static final String UUID_4 = "uuid4";
     private static final String UUID_NOT_EXIST = "dummy";
 
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+    protected static final Resume RESUME_1 = new Resume(UUID_1);
+    protected static final Resume RESUME_2 = new Resume(UUID_2);
+    protected static final Resume RESUME_3 = new Resume(UUID_3);
+    protected static final Resume RESUME_4 = new Resume(UUID_4);
 
     protected AbstractStorageTest(Storage storage) {
         this.storage = storage;
