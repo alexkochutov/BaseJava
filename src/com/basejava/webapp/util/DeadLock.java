c9325f03651574ea4e7ed5f437ea9c6372740dc1
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-07-28 16:53:31 +0400

    fix: Week 11 - Fix issues round 1 	- Extract repeated code of thread creation into dedicated method deadlock(String lock1, String lock2)

--- src/com/basejava/webapp/util/DeadLock.java
+++ src/com/basejava/webapp/util/DeadLock.java
@@ -1,40 +1,29 @@
 package com.basejava.webapp.util;
 
 public class DeadLock {
-    private static final Object LOCK_1 = new Object();
-    private static final Object LOCK_2 = new Object();
+    private static final String LOCK_1 = "LOCK_1";
+    private static final String LOCK_2 = "LOCK_2";
 
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread(() -> {
-            synchronized (LOCK_1) {
-                System.out.println(Thread.currentThread().getName() + ": LOCK_1 is successfully locked");
+        deadlock(LOCK_1, LOCK_2);
+        deadlock(LOCK_2, LOCK_1);
+
+    }
+
+    private static void deadlock(String lock1, String lock2) {
+        new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + ": " + lock1 + " is successfully locked");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                synchronized (LOCK_2) {
-                    System.out.println(Thread.currentThread().getName() + ": LOCK_2 is successfully locked");
+                synchronized (lock2) {
+                    System.out.println(Thread.currentThread().getName() + ": " + lock2 + " is successfully locked");
                 }
             }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            synchronized (LOCK_2) {
-                System.out.println(Thread.currentThread().getName() + ": LOCK_2 is successfully locked");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                synchronized (LOCK_1) {
-                    System.out.println(Thread.currentThread().getName() + ": LOCK_1 is successfully locked");
-                }
-            }
-        });
-
-        thread1.start();
-        thread2.start();
+        }).start();
     }
 }
